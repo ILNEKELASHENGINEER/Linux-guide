14f43e928d438f78fdadf4153049c0636a179994
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-04 23:36:58 +0530

    Change permission for a file
    In this chmod command we can change the permission for file like read, write, execute only or all.
    Hola happy scripting!!!

---
+++ Change_permission.sh
@@ -0,0 +1,5 @@
+#to change the access control
+chmod +x(r)(w)  <filename> #for add execution, read, write permisson respectively
+chmod -x(r)(w)  <filename> #for deleting the execution, read, write permission respectively
+#you can view by using
+ls -lrta
