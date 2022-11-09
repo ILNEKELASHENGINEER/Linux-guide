130f3decd3b9bce9f2bda12010159062954f37f4
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-09 22:14:15 +0530

    Write script in nano and running it.
    Opening the nano text editor and running script written in it.

---
+++ Basic-Bash/nano_script.sh
@@ -0,0 +1,16 @@
+#nano <filename with or without extension>
+#after pressing enter you will enter into nano text editor and you can script your program
+#press x to exit and enter 'y' to confirm the saving process
+#before run the script just do
+#chmod +x filename.<extension>
+#now you can run the script by 
+#./filename.<extension>
+
+nano neke.sh
+echo "HELLO WORLD"
+chmod +x neke.sh
+./neke.sh
+
+#OUTPUT
+#HELLO WORLD
+
